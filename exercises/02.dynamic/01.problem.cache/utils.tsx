4db82a5b3880c7bbc5ac9d6eb4f25aa47477b579
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-02 21:58:47 -0300

    Refactor ship fetching logic by implementing a caching mechanism with shipCache and renaming the getShip function for clarity

--- exercises/02.dynamic/01.problem.cache/utils.tsx
+++ exercises/02.dynamic/01.problem.cache/utils.tsx
@@ -2,16 +2,15 @@ import { type Ship } from './api.server.ts'
 
 export type { Ship }
 
-// 🐨 create a shipCache Map that's got string keys and values are Promise<Ship>
+const shipCache = new Map<string, Promise<Ship>>()
 
-// 🐨 export a new function called getShip (you'll rename the one below).
-//   - it should take a name and optional delay number
-//   - it should check the shipCache for the shipPromise by the name
-//   - if it can't find one, it should call getShipImpl and store the promise in the cache
-//   - then it should return the shipPromise
+export function getShip(name: string, delay?: number) {
+	const shipPromise = shipCache.get(name) ?? getShipImpl(name, delay)
+	shipCache.set(name, shipPromise)
+	return shipPromise
+}
 
-// 🐨 rename this function to getShipImpl and remove the export
-export async function getShip(name: string, delay?: number) {
+async function getShipImpl(name: string, delay?: number) {
 	const searchParams = new URLSearchParams({ name })
 	if (delay) searchParams.set('delay', String(delay))
 	const response = await fetch(`api/get-ship?${searchParams.toString()}`)
